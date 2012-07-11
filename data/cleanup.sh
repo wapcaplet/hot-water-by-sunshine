44d876bf8965f804e3c48f4edd110bc915bb7dd4
Eric Pierce <wapcaplet88@gmail.com>
2012-07-10 18:05:52 -0600

    Script comments

--- data/cleanup.sh
+++ data/cleanup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Clean up log output from ardiff (https://github.com/wapcaplet/ardiff)
+
 echo "Timestamp,Tank,Collector" > temp.txt
 cat $1 | grep . | grep '^[0-9]' | grep -v '\-196' >> temp.txt
 cp temp.txt $1
--- data/graph.sh
+++ data/graph.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+# Graph solar log .csv file using CSVSee (https://github.com/a-e/csvsee)
+
 csvs graph $1.csv \
   -title "Solar Water Heater Performance $1" \
   -ylabel "Temperature (F)" \
